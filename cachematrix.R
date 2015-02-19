6b4e0518c92a090e0dabfa0e0f82ba80858473a1
nhariu <nhariu@webmd.net>
2015-02-19 16:27:06 -0500

    TestOutput.txt added
    Result of the test

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,15 @@
 ## ------------------------------------------------------------------------------------------------------
 ## Assignment 2: Caching the Inverse of a Matrix
 ## ------------------------------------------------------------------------------------------------------
+## Testing Script:
+## size <- 5
+## mt <- matrix(rnorm(size^2), nrow=size, ncol=size)
+## 
+## cache_mt <- makeCacheMatrix(mt)
+## mt.1 <- cacheSolve(cache_mt)
+## mt.2 <- cacheSolve(cache_mt)
+## mt.3 <- cacheSolve(cache_mt)
+## ------------------------------------------------------------------------------------------------------
 
 ## ------------------------------------------------------------------------------------------------------
 ## Function creates a special "matrix" object that can cache its inverse
@@ -9,12 +18,14 @@ makeCacheMatrix <- function(x = matrix())
 {
    m <- NULL
    
+   ## Constructors
    set <- function(y) {
       x <<- y
       m <<- NULL
    }
    get <- function() x
    
+   ## Public functions
    setinverse <- function(solve) m <<- solve(x)
    getinverse <- function() m
    
@@ -30,14 +41,15 @@ makeCacheMatrix <- function(x = matrix())
 ## ------------------------------------------------------------------------------------------------------
 cacheSolve <- function(x, ...)
 {
+   ## Try cached data first
    m <- x$getinverse()
-   
    if(!is.null(m))
    {
       message("getting cached data")
       return(m)
    }
 
+   ## If not there, construct, solve and store (set) it
    data <- x$get()
    m <- solve(data, ...)
    x$setinverse(m)
